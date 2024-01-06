7db528663c697077bb5525d2d0f69c2a3220698f
florianvandenbroeke <florian.vandenbroeke@gmail.com>
2024-01-07 00:19:38 +0100

    Update month

--- Test.py
+++ Test.py
@@ -7,7 +7,7 @@ def text_center(text, image, x, y, font):
     ImageDraw.Draw(image).text((x,y), text, fill="black", font=font)
 
 canvas = Image.new('RGB', (384,500), 'white')
-text_center("Augustus", canvas, 90, 20, chalk)
+text_center("Januari", canvas, 90, 20, chalk)
 text_center("6", canvas, 93, 15, prod)
 
 canvas.show()

0843a5c49eaa2ee740f1188eb76efce441ebb9d7
florianvandenbroeke <florian.vandenbroeke@gmail.com>
2024-01-07 00:11:16 +0100

    Initial commit

---
+++ Test.py
@@ -0,0 +1,14 @@
+from PIL import Image, ImageDraw, ImageFont
+
+chalk = ImageFont.truetype("Franchise.ttf",80)
+prod = ImageFont.truetype("Product Sans Regular", 350)
+
+def text_center(text, image, x, y, font):
+    ImageDraw.Draw(image).text((x,y), text, fill="black", font=font)
+
+canvas = Image.new('RGB', (384,500), 'white')
+text_center("Augustus", canvas, 90, 20, chalk)
+text_center("6", canvas, 93, 15, prod)
+
+canvas.show()
+

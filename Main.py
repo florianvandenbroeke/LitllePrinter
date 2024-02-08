9949327426ff553bcd9e1152dcf4c28e29e98f3b
florianvandenbroeke <florian.vandenbroeke@gmail.com>
2024-02-08 17:56:53 +0100

    Add textwrap functionality

--- Main.py
+++ Main.py
@@ -1,8 +1,5 @@
 from PIL import Image, ImageDraw, ImageFont
-
-width = 384
-
-im = Image.new("RGB", (width, 500), "white")
+import textwrap
 
 wagner = "NT Wagner.otf"
 franchise = "Franchise.ttf"
@@ -16,23 +13,52 @@ retrolight = "Retrolight.ttf"
 london = "Old London.ttf"
 
 
-class DateFront:
+class Frame:
 
-    def __init__(self, image):
-        self.image = image
+    def __init__(self, width=384):
+        self.width = width
+        self.image = Image.new("RGB", (self.width, 500), "white")
         self.d = ImageDraw.Draw(self.image)
         self.current_h = 0
         self.boxcoords = ()
+
+    def paste_im(self, path):
+        to_paste = Image.open(path)
+        self.image.paste(to_paste)
+        self.current_h += to_paste.height
 
     def add_whitespace(self, space):
         self.current_h += space
 
     def draw_text(self, text, font, size):
         font = ImageFont.truetype(font, size)
-        self.d.text((width/2, self.current_h), text, fill="black", font=font, anchor="mt")
-        self.boxcoords = self.d.textbbox((width/2, self.current_h), text, font=font, anchor="mt")
+        self.boxcoords = self.d.textbbox((0, 0), text, font=font)
+        textwidth = self.boxcoords[2] - self.boxcoords[0]
+        self.d.text((self.width/2 - textwidth / 2, self.current_h), text, fill="black", font=font, align="center")
         h_increment = self.boxcoords[3] - self.boxcoords[1]
         self.current_h += h_increment
+
+    def text_wrap(self, text, font, size, max_width):
+
+        # self.d.rectangle((self.width / 2 - max_width / 2, 0, self.width / 2 + max_width / 2, 500), outline="black")
+
+        font_t = ImageFont.truetype(font, size)
+        wordlist = text.split(" ")
+        split_i = 0
+
+        for i in range(len(wordlist)):
+
+            linestring = " ".join(wordlist[split_i:i+1])
+            boxcoords = self.d.textbbox((0, 0), linestring, font=font_t)
+            textwidth = boxcoords[2] - boxcoords[0]
+
+            if textwidth > max_width:
+                wordlist.insert(i, "\n")
+                split_i = i
+
+        self.draw_text(" ".join(wordlist), font, size)
+
+
 
     def draw_underline(self, offfset, w):
         x1, y1 = self.boxcoords[0], self.boxcoords[3] + offfset
@@ -46,27 +72,39 @@ class DateFront:
         self.d.line((x1, y1, x1, y2), fill="black", width=w)
         self.d.line((x1, (y1+y2)//2, 0, (y1+y2)//2), fill="black", width=w)
         self.d.line((x2, y1, x2, y2), fill="black", width=w)
-        self.d.line((x2, (y1+y2)//2, width, (y1+y2)//2), fill="black", width=w)
+        self.d.line((x2, (y1+y2)//2, self.width, (y1+y2)//2), fill="black", width=w)
+
+    def show(self):
+        self.image.show()
 
 
-front = DateFront(im)
+def create_date():
 
-front.add_whitespace(25)
-front.draw_text("JANUARI", harlem_deco, 45)
-front.draw_underline(offfset=22, w=2)
-front.add_whitespace(22)
-front.draw_text("18", market_deco, 200)
-front.add_whitespace(27)
-front.draw_text("Donderdag", franchise, 55)
-front.draw_sidebars(offset=10, w=3)
-front.add_whitespace(25)
-front.draw_text("Daily Times", london, 50)
-front.draw_underline(offfset=3, w=2)
+    front = Frame()
 
-# draw_text(im, 30, "Mei", deco, 65)
-# ImageDraw.Draw(im).line((70, 100, 310, 100), fill="black", width=2)
-# draw_text(im, 120, "14", deco, 200)
-# draw_text(im, 300, "Zondag", franchise, 55)
+    front.add_whitespace(25)
+    front.draw_text("JANUARI", harlem_deco, 45)
+    front.draw_underline(offfset=22, w=2)
+    front.add_whitespace(22)
+    front.draw_text("18", market_deco, 200)
+    front.add_whitespace(27)
+    front.draw_text("Donderdag", franchise, 55)
+    front.draw_sidebars(offset=10, w=3)
+    front.add_whitespace(25)
+    front.draw_text("Dit is een voorbeeld van een tekst die over meerdere ljnen moet komen", london, 50)
+    front.draw_underline(offfset=3, w=2)
+    front.show()
 
-im.show()
-im.save("date.png")
+def create_quote():
+
+    quote = "\"Het leven is een aaneenschakeling van teleurstellingen en dan ga je dood.\""
+
+    frame = Frame()
+
+    frame.paste_im("Images/quote_header.png")
+    frame.add_whitespace(25)
+    frame.text_wrap(quote, product, 30, 270)
+
+    frame.show()
+
+create_quote()

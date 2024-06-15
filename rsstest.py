9381b7542ca55a7eb55853656ac48f4a64d719dc
florianvandenbroeke <florian.vandenbroeke@gmail.com>
2024-06-15 19:23:52 +0200

    Implement news

--- rsstest.py
+++ rsstest.py
@@ -3,13 +3,19 @@ import requests
 
 url = requests.get("https://www.vrt.be/vrtnieuws/nl.rss.articles.xml")
 
-soup = BeautifulSoup(url.content, "lxml")
+soup = BeautifulSoup(url.content, "xml")
 entries = soup.find_all('entry')
 
-for entry in entries:
-    title = entry.title.text
-    summary = entry.summary.text
-    link = entry.link['href']
-    print(title)
-    print(summary)
-    print(link)
+titles = [entry.title.text for entry in entries]
+summaries = [entry.summary.text for entry in entries]
+items = [(entry.title.text, entry.summary.text) for entry in entries]
+
+print(items)
+
+# for entry in entries:
+#     title = entry.title.text
+#     summary = entry.summary.text
+#     link = entry.link['href']
+#     print(title)
+#     print(summary)
+#     print(link)

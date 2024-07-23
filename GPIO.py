2ef67bb48e0d76f4802645be8bed45179c4ec2f8
florianvandenbroeke <florian.vandenbroeke@gmail.com>
2024-07-23 14:44:00 +0200

    Re-implement GPIO buttons

--- GPIO.py
+++ GPIO.py
@@ -7,17 +7,17 @@ list_button = Button(3)
 calendar_button = Button(4)
 
 
-# def gpio():
-#     while True:
-#
-#         if daily_button.is_pressed:
-#             print_daily()
-#             sleep(2)
-#
-#         if list_button.is_pressed:
-#             print_list()
-#             sleep(2)
-
 def gpio():
-    daily_button.when_pressed = print_daily
-    list_button.when_pressed = print_list
+    while True:
+
+        if daily_button.is_pressed:
+            print_daily()
+            sleep(2)
+
+        if list_button.is_pressed:
+            print_list()
+            sleep(2)
+
+# def gpio():
+#     daily_button.when_pressed = print_daily
+#     list_button.when_pressed = print_list
